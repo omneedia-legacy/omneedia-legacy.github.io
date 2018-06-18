305c49ad463b93464a901c9b94aab369169ae462
Stéphane Zucatti <stephane.zucatti@me.com>
2018-06-18 16:07:50 +0200

    FIX: loader engine 0.8.1

--- omneedia/0.8.1/Localizer.js
+++ omneedia/0.8.1/Localizer.js
@@ -1,79 +1,75 @@
-
-Lang = function(x)
-{
+Lang = function (x) {
 	try {
 		return i18n[Culture.LANG][x];
-	} catch(ex) {
+	} catch (ex) {
 		return x;
 	}
 };
 
-_ = function(x)
-{
+_ = function (x) {
 	try {
 		return i18n[Culture.LANG][x];
-	} catch(ex) {
+	} catch (ex) {
 		return x;
 	}
 };
 
 Ext.define("omneedia.Localizer", {
-    
-    singleton : true,	
+
+	singleton: true,
 	isLoaded: false,
-	
-	LANG : "",
+
+	LANG: "",
 	ULANG: "",
-	
-	init: function()
-	{
-		try{
+
+	init: function () {
+		try {
 			if (Settings.DEBUG) i18n = {};
 			if (Settings.DEBUG) i18n_framework = {};
-		}catch(e){};
-		
-		Array.prototype.inArray = function(p_val) {
+		} catch (e) {};
+
+		Array.prototype.inArray = function (p_val) {
 			return (this.indexOf(p_val) < -1);
 		};
-		
+
 		Culture.ULANG = (navigator.language) ? navigator.language : navigator.userLanguage;
-		
-		Culture.ULANG=Culture.ULANG.split('-')[0];
-		
-		if (LANGS.indexOf(Culture.ULANG)>-1) {
-			Culture.LANG=Culture.ULANG;
+
+		Culture.ULANG = Culture.ULANG.split('-')[0];
+
+		if (LANGS.indexOf(Culture.ULANG) > -1) {
+			Culture.LANG = Culture.ULANG;
 		} else {
-			Culture.LANG=LANGS[0];
+			Culture.LANG = LANGS[0];
 		};
-		
-		if (window.localStorage['LANG']) Culture.LANG=window.localStorage['LANG'];
-		
+
+		if (window.localStorage['LANG']) Culture.LANG = window.localStorage['LANG'];
+
 	},
-	update: function(fn)
-	{
+	update: function (fn) {
 
 		if (Culture.isLoaded) fn();
 		else {
 			if (Culture.LANG) {
-				var url=["Contents/Culture/"+Culture.LANG+".js"];
-				if (!Settings.CDN) var CDN="http://omneedia.github.io/cdn"; else var CDN=Settings.CDN;
-				var url2=[CDN+"/framework.lang/"+Settings.TYPE+"/ext-lang-"+Culture.LANG+".js"];
+				var url = ["Contents/Culture/" + Culture.LANG + ".js"];
+				if (!Settings.CDN) var CDN = "https://cdn.omneedia.com";
+				else var CDN = Settings.CDN;
+				var url2 = [CDN + "/omneedia/0.8.1/framework.lang/" + Settings.TYPE + "/ext-lang-" + Culture.LANG + ".js"];
 				if (Settings.DEBUG) {
-					Require(url2,function(){
+					Require(url2, function () {
 						i18n_framework[Culture.LANG]();
-						Require(url,function(){
-							Culture.isLoaded=true;
-							if (fn) fn();					
+						Require(url, function () {
+							Culture.isLoaded = true;
+							if (fn) fn();
 						});
 					});
 				} else {
-					Culture.isLoaded=true;
+					Culture.isLoaded = true;
 					i18n_framework[Culture.LANG]();
-					if (fn) fn();				
+					if (fn) fn();
 				}
 			} else {
-				Culture.isLoaded=true;
-				if (fn) fn();		
+				Culture.isLoaded = true;
+				if (fn) fn();
 			}
 		}
 	}
