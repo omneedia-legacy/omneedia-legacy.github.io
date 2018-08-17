90e15dc6e7965e73d147236d6ac465670c52f570
Stéphane Zucatti <stephane.zucatti@me.com>
2018-08-17 10:26:27 +0200

    small fixes

---
+++ omneedia/0.9.2/extjs.modules.js
@@ -0,0 +1,11 @@
+[
+    "libs/Ext/GlobalMenu.js",
+    "libs/IO.js",
+    "libs/Localizer.js",
+    "libs/Query.js",
+    "libs/Ext/Lib.js",
+    "libs/Ext/Loader.js",
+    "libs/Auth.js",
+    "libs/DB.js",
+    "libs/Notification.js"
+]
---
+++ omneedia/0.9.2/onsenui-mobile.modules.js
@@ -0,0 +1,9 @@
+[
+    "libs/IO.js",
+    "libs/Localizer.js",
+    "libs/Query.js",
+    "libs/Auth.js",
+    "libs/DB.js",
+    "libs/Mobile/Lib-onsenui.js",
+    "libs/VueComponent.js"
+]
